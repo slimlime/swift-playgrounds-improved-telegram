616527db8af8af271e190e8afd67c3cc5ca37f54
SLimma <30815731+slimlime@users.noreply.github.com>
2018-12-03 17:24:43 +1000

    `mutating func `for struct to adopt protocol that mutates self property.

--- SwiftLearnings/SwiftTour-ProtocolsAndExtensions.playground/Contents.swift
+++ SwiftLearnings/SwiftTour-ProtocolsAndExtensions.playground/Contents.swift
@@ -1,6 +1,32 @@
 
 protocol ExampleProtocol {
     var simpleDescription: String { get }
+    // mutating func required for methods that modify Struct properties... classes are fine...
     mutating func adjust()
 }
 
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A simple class"
+    var anotherProperty: Int = 69105
+    
+    func adjust() {
+        simpleDescription += " Now 100% adjusted."
+    }
+}
+
+var a = SimpleClass()
+a.simpleDescription
+a.adjust()
+let aDescription = a.simpleDescription
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    // struct self normally immutable without `mutating func`
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+
+var b = SimpleStructure()
+b.simpleDescription
+b.adjust()
+let bDescription = b.simpleDescription
