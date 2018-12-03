95fe3ff73a9e4a3499712c48c1c8bf0d564862d9
SLimma <30815731+slimlime@users.noreply.github.com>
2018-12-03 14:17:15 +1000

    Variant of shapes example.

--- SwiftLearnings/SwiftTour-ObjectsClasses.playground/Contents.swift
+++ SwiftLearnings/SwiftTour-ObjectsClasses.playground/Contents.swift
@@ -16,4 +16,73 @@ class Shape {
 
 let triangle = Shape(numSides: 3)
 let triangleSideDesc = triangle.simpleDescription()
+enum RegularSidedShapes: Int {
+    case circle = 1
+    case triangle = 3
+    case square = 4 // irregular squares rectangles rhombus blatrapezi
+    case pentagon = 5
+    case hexagon = 6
+    case heptagon, septagon, octagon, nonagon, decagon, undecagon, dodecagonnnnn
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .circle:
+            return "circle"
+        case .triangle:
+            return "triangle"
+        case .square:
+            return "square"
+        default:
+            return String(self.rawValue) // .... raw valuekey or?
+        }
+    }
+}
+class NamedShape: Shape {
+    let name: String
+    
+    init(numSides: Int, name: String) {
+        self.name = name
+        super.init(numSides: numSides)
+    }
+    override func simpleDescription() -> String {
+        return "A \(name) with \(numberOfSides) sides"
+    }
+}
+
+let square = NamedShape(numSides: 4, name: "Square")
+let squareDesc = square.simpleDescription()
+class Square: NamedShape {
+    let sideLength: Double
+    
+    init(sideLength: Double) {
+        self.sideLength = sideLength
+        super.init(numSides: 4, name: "Square")
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A \(name) with sides of length \(sideLength) and area \(area())"
+    }
+}
+class Circle: NamedShape {
+    let radius: Double
+    
+    init(radius: Double) {
+        self.radius = radius
+        super.init(numSides: 1, name: "Circle")
+    }
+    func area() -> Double {
+        return Double.pi * (radius * radius) // pi * r^2
+    }
+    override func simpleDescription() -> String {
+        return "A \(name) with radius \(radius) and area \(area())"
+    }
+}
+let coolSquare = Square(sideLength: 3)
+let coolSqDesc = coolSquare.simpleDescription()
+let coolCircle = Circle(radius: 4)
+let coolCiDesc = coolCircle.simpleDescription()
 
