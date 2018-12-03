569d2f7f3ce3eaf08822c1306f52ee46d270abc2
SLimma <30815731+slimlime@users.noreply.github.com>
2018-12-03 13:47:34 +1000

    Triangle shape...

--- SwiftLearnings/SwiftTour-ObjectsClasses.playground/Contents.swift
+++ SwiftLearnings/SwiftTour-ObjectsClasses.playground/Contents.swift
@@ -1,3 +1,19 @@
-import UIKit
 
-var str = "Hello, playground"
+// Objects and Classes
+
+class Shape {
+    let numberOfSides: Int
+    // static let vmShapeSide = "" // interesting
+    
+    init(numSides n: Int) {
+        self.numberOfSides = n
+    }
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides"
+    }
+}
+
+
+let triangle = Shape(numSides: 3)
+let triangleSideDesc = triangle.simpleDescription()
+
