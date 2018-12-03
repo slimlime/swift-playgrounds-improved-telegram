2c5b9070a7683b04f52f44d2752efaadba7c69c9
SLimma <30815731+slimlime@users.noreply.github.com>
2018-12-03 15:13:45 +1000

    Card rank enum case and raw values.

--- SwiftLearnings/SwiftTour-Enum.playground/Contents.swift
+++ SwiftLearnings/SwiftTour-Enum.playground/Contents.swift
@@ -1,1 +1,22 @@
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen
+    case king = 13
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+            
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.ace // string case
+let aceRawVal = ace.rawValue // number value
+let threeCard = Rank.three // string case "three"
+let threeCardRawVal = threeCard.rawValue // raw num 3
 
